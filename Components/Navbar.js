21260633bac98a0a03123c8eb96b32840e323235
Jeremy <st.jeremy1@gmail.com>
2022-11-17 11:06:15 +0100

    v1.4

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AiOutlineMenu from 'react-icons/ai';
 
 import {
   Drawer,
@@ -12,11 +13,32 @@ import {
   Radio,
   Stack,
   Button,
-  useDisclosure
+  useDisclosure,
+  IconButton
 } from '@chakra-ui/react';
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  ChevronDownIcon,
+  HamburgerIcon,
+  ExternalLinkIcon,
+  RepeatIcon,
+  EditIcon,
+  AddIcon
+} from '@chakra-ui/react'
+
+
 import React from "react";
 
 const Navbar = () => {
+
   function PlacementExample() {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [placement, setPlacement] = React.useState('right')
@@ -26,9 +48,11 @@ const Navbar = () => {
         <RadioGroup defaultValue={placement} onChange={setPlacement}>
           
         </RadioGroup>
-        <Button colorScheme='blue' onClick={onOpen}>
-          Open
+        <Button colorScheme='blue' onClick={onOpen} >
+        Shade
         </Button>
+        
+
         <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
           <DrawerOverlay />
           <DrawerContent>
--- pages/index.js
+++ pages/index.js
@@ -22,40 +22,69 @@ export default function Home() {
         <link rel="icon" href="/public/sendme-logo.png" />
       </Head>
 
-      <div className={ styles.content}>
-        <div className="header">
-          <h1>Get The <span>Best meat</span>  in <span>30</span>Mins</h1>
-          <p>Focus on the things that matter while we handle your animal protein needs by giving you premium quality meat at the best prices.
+      <div className={ styles.content }>
+        <div className={ styles.header }>
+          <h1 className={ styles.mainHeading }>Get The <span className={ styles.red }>Best meat</span>  in <span  className={ styles.grey }>30</span>mins</h1>
+
+          <p className={ styles.grey }>Focus on the things that matter while we handle your animal protein needs by giving you premium quality meat at the best prices.
           </p>
+          <br />
+
           <input type="button" value="Order meat on web now" />
-          <p>or get the app</p>
-          <Image src='/app-store.svg' width={142}  height={200} alt='app store'/>
-          <Image src='/google-play.svg' width={142}  height={200} alt='google play'/>
+          <br />
+          <br />
+
+          <p className={ styles.black }>or get the app</p>
+          <div className={ styles.store }>
+            <Image src='/app-store.png' width={142}  height={41} alt='app store'/>
+            <Image src='/google-play.png' width={142}  height={41} alt='google play'/>
+          </div>
         </div>
 
-        <div className='animalProtein'>
+        <div className={styles.comingSoon}>
+          <Image src='/coming-soon.png' width={700}  height={51} alt='google play'/>
+        </div>
+
+        <div className={styles.animalProtein}>
           <h1>Buy Animal Proteins</h1>
           <p>All your favorite meat in one stop </p>
-          <Image src='/app-store.svg' width={142}  height={200} alt='app store'/>
-          <Image src='/google-play.svg' width={142}  height={200} alt='google play'/>
-          <Image src='/app-store.svg' width={142}  height={200} alt='app store'/>
-          <Image src='/google-play.svg' width={142}  height={200} alt='google play'/>
+
+          <div className={styles.products}>
+            <div className={ styles.box }>
+              <Image src='/goat.png' width={89}  height={89} alt='goat'/>
+              <p>Goat</p>
+            </div>
+            <div className={ styles.box }>
+              <Image src='/cow.png' width={89}  height={89} alt='cow'/>
+              <p>Cow</p>
+            </div>
+            <div className={ styles.box }>
+              <Image src='/fish.png' width={89}  height={89} alt='fish'/>
+              <p>Fish</p>
+            </div>
+              <div className={ styles.box }>
+              <Image src='/chicken.png' width={89}  height={89} alt='chicken'/>
+              <p>Chicken</p>
+            </div>
+          </div>
         </div>
 
-        <div className="B2B">
-          <h1>We Also Sell Meat to businesses </h1>
+        <div className={styles.B2B}>
+          <h1>We Also Sell Meat to Businesses </h1>
           <p>We currently deliver a variety of the best animal protein to over 100 restaurants and hotels, helping them meet their business needs by creating the best meat experience for their customers and increasing their sales and profit margin.
           </p>
 
-          <Image src='/google-play.png' width={142}  height={200} alt='google play'/>
-          <Image src='/google-play.png' width={142}  height={200} alt='google play'/>
-          <Image src='/google-play.png' width={142}  height={200} alt='google play'/>
-          <Image src='/google-play.png' width={142}  height={200} alt='google play'/>
+          <div className={styles.businesses}>
+            <Image src='/spar-logo.png' width={88}  height={26} alt='SPAR logo'/>
+            <Image src='/kili-logo.png'  width={88}  height={26} alt='Kilimanjaro logo'/>
+            <Image src='/chicken-logo.png'  width={88}  height={26} alt='Chicken and Co logo'/>
+            <Image src='/B-logo.png'  width={88}  height={26} alt='B-logo'/>
+          </div>
 
           <a href="">Order meat for your business</a>
         </div>
 
-        <div className="earnWithSendme">
+        <div className={styles.earnWithSendme}>
           <h1>Would you like to Earn With Sendme ?</h1>
           <p>You can make money just by buying sendme products on behalf of your friends, colleagues, or neighbors.</p>
           <p>You can earn:</p>
