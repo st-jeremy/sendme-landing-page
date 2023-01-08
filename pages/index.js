e20ed8473779ed6c115942778579fad00f9af795
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-08 22:27:05 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+
 import styles from '../styles/Home.module.css';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { BsArrowUpRight } from 'react-icons/bs';
